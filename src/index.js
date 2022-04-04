dd0d5d6d1fb22376a804eadc0f06620fdd23e75a
arnesj01 <joshua.arneson@mssm.edu>
2022-04-04 18:40:27 -0500

    still learning

--- src/index.js
+++ src/index.js
@@ -7,10 +7,19 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use((req, res, next) => {
-    console.log(req.method, req.path)
-    next()
-})
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+//     console.log(req.method, req.path)
+//     next()
+// })
+
+// app.use((req, res, next) => {
+//     req.statusCode(503).send('Site is currently down for maintenance. Check back soon!')
+// })
 
 app.use(express.json())
 app.use(userRouter)
